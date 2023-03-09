eac6b62cf5c238044b8a1b8594a86894fc56f7ae
JorgeHenriqueBorgesSantos <120414588+JorgeHenriqueBorgesSantos@users.noreply.github.com>
2023-03-09 17:39:31 -0400

    Add files via upload

--- Ponteiros 2/20230307_005.c
+++ Ponteiros 2/20230307_005.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void Ordenar(int Lis[]){
+    int aux;
+    int *k, *p, *z;
+
+    k = &Lis[0];
+    p = &Lis[1];
+    z = &Lis[2];
+
+    if (*k > *p){
+        aux = *k;
+        *k = *p;
+        *p = aux;
+    }
+
+    if (*k > *z){
+        aux = *k;
+        *k = *z;
+        *z = aux;
+    }
+
+    if (*p > *z){
+        aux = *p;
+        *p = *z;
+        *z = aux;
+    }
+}
 
 int main()
 {
@@ -13,6 +39,8 @@ int main()
         scanf("%d", &Faustao);
         *Domingao = Faustao;
     }
+
+    Ordenar(Tv_Br);
 
     for (Domingao = &Tv_Br[0]; Domingao <= &Tv_Br[2]; Domingao++){
         printf("%d ", *Domingao);
